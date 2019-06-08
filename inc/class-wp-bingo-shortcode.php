4f14b38d984afb3075da65705db40a2ec1fd41f8
Miguel A. Estrada <mae829@gmail.com>
2019-06-08 11:46:48 -0700

    Clean up files to abide by WP code standards

--- inc/class-wp-bingo-shortcode.php
+++ inc/class-wp-bingo-shortcode.php
@@ -2,31 +2,40 @@
 /**
  * The plugin class to add our custom shortcode.
  */
+
+/**
+ * WP_Bingo_Shortcode
+ */
 class WP_Bingo_Shortcode {
 
 	/**
-	 * A reference to an instance of this class.
+	 * Instance of this class
+	 *
+	 * @var boolean
 	 */
 	private static $instance = false;
 
 	/**
-	 * Singleton
+	 * Constructor
 	 *
-	 * Returns a single instance of the current class.
+	 * - Used in the standard way and to defines all the WordPress actions and filters used by this theme
 	 */
-	public static function singleton() {
-
-		if ( !self::$instance )
-			self::$instance = new self;
-
-		return self::$instance;
+	public function __construct() {
+		// Register the wp_bingo shortcode.
+		add_shortcode( 'wp_bingo', array( $this, 'bingo_shortcode' ) );
 	}
 
-	public function __construct() {
+	/**
+	 * Singleton
+	 *
+	 * Returns a single instance of this class.
+	 */
+	public static function singleton() {
+		if ( ! self::$instance ) {
+			self::$instance = new self();
+		}
 
-		// Register the wp_bingo shortcode
-		add_shortcode( 'wp_bingo', array( $this, 'bingo_shortcode' ) );
-
+		return self::$instance;
 	}
 
 	/**
@@ -35,59 +44,58 @@ class WP_Bingo_Shortcode {
 	 * @return  string  Contains the markup for the bingo table
 	 */
 	public function bingo_shortcode() {
+		// Get required metadata.
+		$post_ID   = get_the_ID();
+		$buzzwords = get_post_meta( $post_ID, '_bingo_buzzwords', true );
 
-		// Get required metadata
-		$post_ID		= get_the_ID();
-		$buzzwords		= get_post_meta( $post_ID, '_bingo_buzzwords', true );
+		// Header Word.
+		$header_word = 'BINGO';
+		$header_word = apply_filters( 'wp_bingo_header_word', $header_word );
 
-		// Header Word
-		$header_word	= 'BINGO';
-		$header_word	= apply_filters( 'wp-bingo-header-word', $header_word );
+		$header_word_array = str_split( $header_word, 1 );
 
-		$header_word_array	= str_split( $header_word, 1 );
-
-		// Call up the needed style/script files
+		// Call up the needed style/script files.
 		wp_enqueue_style( 'wp-bingo' );
 		wp_enqueue_script( 'wp-bingo' );
 
-		// Start getting content of shortcode ready
+		// Start getting content of shortcode ready.
 		ob_start();
+		?>
 
-		echo '<div class="wp-bingo__header">';
+		<div class="wp-bingo__header">
+			<?php
 			foreach ( $header_word_array as $letter ) {
-				echo '<div>'. $letter .'</div>';
+				echo '<div>' . esc_html( $letter ) . '</div>';
 			}
-		echo '</div>';
+			?>
+		</div>
 
-		echo '<div class="wp-bingo__wrapper">';
+		<div class="wp-bingo__wrapper">
+			<?php
+			if ( ! empty( $buzzwords ) ) {
 
-			if ( !empty( $buzzwords ) ) {
-
-				$count_words    = count( $buzzwords );
+				$count_words = count( $buzzwords );
 
 				for ( $i = 0; $i < $count_words; $i++ ) {
 
-					// FREE TILE
-					if ( $i == 12 ) {
-						echo '<div class="wp-bingo__item active">FREE</div>'."\n\t\t\t\t";
+					// FREE TILE.
+					if ( 12 === $i ) {
+						echo '<div class="wp-bingo__item active">FREE</div>' . "\n\t\t\t\t";
 					}
 
-					$word   = array_rand( $buzzwords );
+					$word = array_rand( $buzzwords );
 
-					echo '<div class="wp-bingo__item">'. $buzzwords[$word] .'</div>'."\n\t\t\t\t";
+					echo '<div class="wp-bingo__item">' . esc_html( $buzzwords[ $word ] ) . '</div>' . "\n\t\t\t\t";
 
-					unset( $buzzwords[$word] );
+					unset( $buzzwords[ $word ] );
 
 				}
-
 			} else {
 				echo 'NEED BUZZWORDS TO BE POPULATED';
 			}
-
-		echo '</div>';
-
+			?>
+		</div>
+		<?php
 		return ob_get_clean();
-
 	}
-
 }
--- inc/template-bingo-theme.php
+++ inc/template-bingo-theme.php
@@ -1,19 +1,27 @@
 <?php
-	/**
-	 * Template Name: Bingo - Theme
-	 * Template Post Type: page
-	 */
-	get_header();
+/**
+ * Template Name: Bingo - Theme
+ * Template Post Type: page
+ */
+
+get_header();
 ?>
 		<div class="wrapper">
 
 			<h1><?php the_title(); ?></h1>
 
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<div class="description"><?php the_content(); ?></div>
-			<?php endwhile; endif; ?>
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+					the_post();
+					?>
+					<div class="description"><?php the_content(); ?></div>
+					<?php
+				endwhile;
+			endif;
+			?>
 
-			<?php echo do_shortcode('[wp_bingo]'); ?>
+			<?php echo do_shortcode( '[wp_bingo]' ); ?>
 
 		</div>
 
